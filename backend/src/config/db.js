148d5193724a3093baeae0fe1f9b2f59d0101204
Omkar Kathile <omkarkathile420@gmail.com>
2026-07-06 15:56:18 +0530

    feat: setup Express app, server bootstrap & MongoDB connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dns from "node:dns";
+
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const connectDB = async () => {
+
+    try{
+        const conn=await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1);
+    }
+
+}
+
+export default connectDB;
